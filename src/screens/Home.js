eeab484e0ac9349272f385c7134e2ed6200746c9
jaybee4real <okorojijaybee1@gmail.com>
2020-06-06 09:21:00 +0100

    _fecth data from youtube api

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,15 +1,46 @@
-import React from "react";
-import { StyleSheet, Text, View, Image, TouchableOpacity, ScrollView } from "react-native";
+import React, { useState , useEffect} from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+  ScrollView,
+  FlatList
+} from "react-native";
 import Card from "../components/Card";
 import Navbar from "../components/Navbar";
 
+//
+
 const Home = () => {
+  const [CardData, setCardData] = useState([
+    //
+  ]);
+
+
+  const fetchData = () => {
+    fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&q=video&type=video&key=AIzaSyCXoIdJ9u4w-IttlR_bbsVd87M0-ffd02Q`)
+      .then(data => data.json())
+      .then(data => {
+        setCardData(data)
+        console.log(data)
+    })
+
+      .catch(err => {
+      console.log(`this is the error ${error}`)
+    })
+  }
+
+  useEffect(() => {
+    fetchData()
+    }, [])
   return (
     <View style={styles.container}>
       <Navbar />
-      <ScrollView>
-        <Card />
-      </ScrollView>
+      {/* <FlatList data={CardData} renderItem={(item) => {
+        return <Card />
+      }}/> */}
     </View>
   );
 };
